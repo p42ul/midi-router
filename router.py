0a9869400a9817926ec6c0acd6ee2f7dd93376e6
paul <paul@plebian.badcore.org>
2020-04-24 13:43:47 -0400

    Route pitchbend, aftertouch, and clock.

--- router.py
+++ router.py
@@ -20,10 +20,18 @@ run(
         # Print('pre', 'in') >>
         [
             PortFilter('bs2in') >> [
-                Filter(NOTE) >> Output('dtout', 10),
-                Filter(CTRL|PITCHBEND|AFTERTOUCH) >> Output('bs2out', 12)
+                # We send aftertouch to the Digitone, and Modwheel to the BSII.
+                # It's not a perfect solution, but because the Digitone doesn't
+                # route Pitchbend or Aftertouch, we have to compromise.
+                Filter(NOTE|PITCHBEND|AFTERTOUCH) >> Output('dtout', 10),
+                Filter(CTRL|PITCHBEND) >> Output('bs2out', 12)
                 ],
-            PortFilter('dtin') >> ChannelFilter(12) >> Output('bs2out', 12),
-            ]
+            # Send messages on Channel 12 back to the BSII,
+            # as well as Sync.
+            PortFilter('dtin') >> [
+                    ChannelFilter(12),
+                    Filter(SYSRT_CLOCK),
+            ] >> Output('bs2out', 12)
+        ]
         # >> Print('post', 'out')
         )
