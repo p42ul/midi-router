5b5a45ac77aeba9d6e7771588272dbdd58086077
Paul Buser <buser.paul@gmail.com>
2020-12-04 08:39:18 -0500

    Add Digitakt support.

--- router.py
+++ router.py
@@ -7,10 +7,12 @@ config(
         in_ports = [
             ('bs2in', '.*Bass Station II.*'),
             ('dtin',  '.*Elektron Digitone.*'),
+            ('rdin',  '.*RD Series MIDI 1.*'),
             ],
         out_ports = [
             ('bs2out', '.*Bass Station II.*'),
             ('dtout',  '.*Elektron Digitone.*'),
+            ('dkout',  '.*Elektron Digitakt.*'),
             ('mioout', '.*mio.*'),
             ('rdout',  '.*RD Series MIDI 1.*'),
             ],
@@ -28,15 +30,17 @@ run(
                 # route Pitchbend or Aftertouch, we have to compromise.
                 [Filter(NOTE|PITCHBEND|AFTERTOUCH), CtrlFilter(64)] >> Output('dtout', 10),
                 Filter(CTRL|PITCHBEND) >> Output('bs2out', 12)
-            ],
+                ],
             # Send messages on Channel 12 back to the BSII,
             # as well as Sync.
             PortFilter('dtin') >> [
                     ChannelFilter(12),
                     Filter(SYSRT_CLOCK),
             ] >> Output('bs2out', 12),
-            PortFilter('dtin') >> Filter(SYSRT) >> Output('mioout', 1),
+            # Send SYNC to Mio and Digitakt.
+            PortFilter('dtin') >> Filter(SYSRT) >> [Output('mioout', 1), Output('dkout', 1)],
+            # Send notes and CCs on channel 2 to the Roland.
             PortFilter('dtin') >> ChannelFilter(2) >> Output('rdout', 1),
         ]
         # >> Print('post', 'out')
-)
+        )
